528e8d4f077c2374cc56ab6ff9494267a7feb567
Andrei5090 <andrei5090@gmail.com>
2025-07-17 14:42:06 +0200

    Add starter spring app

---
+++ src/main/java/com/xyzbank/digital_onboarding_api/DigitalCustomerOnboardingApiApplication.java
@@ -0,0 +1,13 @@
+package com.xyzbank.digital_onboarding_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCustomerOnboardingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCustomerOnboardingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xyzbank/digital_onboarding_api/DigitalCustomerOnboardingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyzbank.digital_onboarding_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalCustomerOnboardingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
